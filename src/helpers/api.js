31254e4d4bd406a89c69c56a617e7b34e1b3df62
Jack Tran <jack@Keentee-2.local>
2020-11-12 23:19:29 +0700

    Fix bugs

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 5000,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
